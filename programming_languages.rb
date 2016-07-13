d2b795b068a31d226bc7dc7bd6445073af6dddf2
Schyler Hosler <schylerhosler@gmail.com>
2016-07-13 21:00:55 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,15 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |lang_style, lang_name|
     lang_name.each do |lang_type, lang_func|
-      new_hash[lang_name] = lang_type
-      new_hash[lang_name][:style] = []
-      new_hash[lang_name][:style] << lang_style
+      new_hash[lang_type] = lang_func
+      new_hash[lang_type][:style] = []
+      #new_hash[lang_type][:style] << lang_style
+      #*new_hash[lang_type][:style] #can this work...?
+    end
+  end
+  languages.each do |lang_style, lang_name|
+    lang_name.each do |lang_type, lang_func|
+      new_hash[lang_type][:style] << lang_style
     end
   end
   return new_hash
